836a7ec63c067dfbd5b8ad210a0f1e1807ec8b4b
seyfe16 <58422187+seyfe16@users.noreply.github.com>
2020-02-03 07:28:55 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,57 +1,59 @@
-<?php get_header() ?>
-<div class="containe main-body"  style="margin-bottom: 150px;">
-  <div class="row">
-      <div class="col-8">
-        <?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : the_post(); ?>
-          <div class="card posts">
-            <div class="card-header">
-              <?php the_title() ?>
-            </div>
-            <div class="card-body">
-            <blockquote class="blockquote mb-0">
-              <div class="row">
-                <div class="col-6">
-                  <?php
-                  if ( has_post_thumbnail() ) { ?>
-                    <figure class="figure">
-                      <img src="<?php the_post_thumbnail_url(); ?>" class="figure-img img-fluid z-depth-1"
-                        alt="..." style="width: 400px; height: 250px">
-                    </figure>
-                  <?php } else {?>
-                    <figure class="figure">
-                      <img src="https://www.unesale.com/ProductImages/Large/notfound.png" class="figure-img img-fluid z-depth-1"
-                        alt="..." style="width: 400px; height: 250px">
-                    </figure>
-                  <?php } ?>
-                </div>
-                <div class="col-6">
-                  <p><?php the_excerpt() ?></p>
-                </div>
-              </div>
-              <footer class="blockquote-footer"><?php the_time('F j, Y g:i a') ?>- By <?php the_author() ?><cite title="Source Title">
-                <a href="<?php the_permalink() ?>">See More</a>
-              </cite></footer>
-            </blockquote>
-          </div>
-        </div>
-        <?php endwhile; ?>
-      <?php endif; ?>
-      </div>
-      <div class="col-4" style="margin-top: 17px;">
-        <button class="btn btn-outline-default btn-sm fixed-action-btn" style="bottom: 45px; right: 24px;" type="button" data-toggle="collapse" data-target="#collapseExample"
-          aria-expanded="false" aria-controls="collapseExample">
-          Side menus <strong> >> </strong>
-        </button>
-        <div class="collapse" id="collapseExample">
-          <div class="mt-3">
-            <?php if (is_active_sidebar('sidebar')) {
-              dynamic_sidebar('sidebar');
-            } ?>
-          </div>
-        </div>
-      </div>
-  </div>
-</div>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package SF-THEME
+ */
 
-<?php get_footer() ?>
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		if ( have_posts() ) :
+
+			if ( is_home() && ! is_front_page() ) :
+				?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+				<?php
+			endif;
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				/*
+				 * Include the Post-Type-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_type() );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
