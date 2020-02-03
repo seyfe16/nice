9dca44a77e3187e735099b74707a993b3fe3f9ab
seyfe16 <58422187+seyfe16@users.noreply.github.com>
2020-02-03 07:33:10 +0000

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,38 +1,161 @@
 <?php
-  require_once('wp-bootstrap-navwalker.php');
+/**
+ * SF-THEME functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package SF-THEME
+ */
 
+if ( ! function_exists( 'sf_theme_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
+	 */
+	function sf_theme_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on SF-THEME, use a find and replace
+		 * to change 'sf-theme' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'sf-theme', get_template_directory() . '/languages' );
 
-  function wpb_theme_setup(){
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
 
-    add_theme_support( 'post-thumbnails' );
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
 
-    register_nav_menus( array(
-      'primary' => __('Primary Menu'),
-      'footer' => __('Footer Menu')
-    ));
-  }
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
 
-  function wpt_theme_styles(){
-    wp_enqueue_style('main_css', get_template_directory_uri().'/style.css');
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus( array(
+			'menu-1' => esc_html__( 'Primary', 'sf-theme' ),
+		) );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'sf_theme_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support( 'custom-logo', array(
+			'height'      => 250,
+			'width'       => 250,
+			'flex-width'  => true,
+			'flex-height' => true,
+		) );
 	}
+endif;
+add_action( 'after_setup_theme', 'sf_theme_setup' );
 
-  function set_excerpt_length(){
-    return 35;
-  }
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function sf_theme_content_width() {
+	// This variable is intended to be overruled from themes.
+	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+	$GLOBALS['content_width'] = apply_filters( 'sf_theme_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'sf_theme_content_width', 0 );
 
-  function wpt_init_widgets($id){
-    register_sidebar(array(
-      'name' => 'Sidebar' ,
-      'id' => 'sidebar',
-      'before_widget' => '<div class="main-body">',
-      'after_widget' => '</div>',
-      'before_title' => '</h5>',
-      'after_title' => '</h5>'
-   ));
-  }
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function sf_theme_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar', 'sf-theme' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', 'sf-theme' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'sf_theme_widgets_init' );
 
-  add_filter('excerpt_length', 'set_excerpt_length');
-  add_action('after_setup_theme','wpb_theme_setup');
-  add_action('wp_enqueue_scripts', 'wpt_theme_styles');
-  add_action('widgets_init', 'wpt_init_widgets');
- ?>
+/**
+ * Enqueue scripts and styles.
+ */
+function sf_theme_scripts() {
+	wp_enqueue_style( 'sf-theme-style', get_stylesheet_uri() );
+
+	wp_enqueue_script( 'sf-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'sf-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'sf_theme_scripts' );
+
+/**
+ * Implement the Custom Header feature.
+ */
+require get_template_directory() . '/inc/custom-header.php';
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
+
