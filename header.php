c8ea486d33b7360f7b72f4ffdb878c99d612c737
seyfe16 <58422187+seyfe16@users.noreply.github.com>
2020-02-03 07:59:14 +0000

    Add files via upload

--- header.php
+++ header.php
@@ -1,54 +1,58 @@
-<!DOCTYPE html>
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package SF-THEME
+ */
+
+?>
+<!doctype html>
 <html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="<?php bloginfo('charset') ?>">
-    <meta name="description" content="<?php bloginfo('description') ?>">
-    <title>
-      <?php bloginfo('name')?> |
-      <?php is_front_page() ? bloginfo('description') :  wp_title()?>
-    </title>
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/mdb.min.css">
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-    <style media="screen">
-    .submit{
-      background: transparent;
-      color: #212121;
-      border: 0.5px solid #dddddd;
-      padding: 5px 20px;
-      border-radius: 3px;
-    }
-    .submit:hover{
-      box-shadow: 0 0 2px rgba(35, 173, 255, 1);
-    }
-    </style>
-  </head>
-  <?php wp_head() ?>
-  <body>
-      <nav class="navbar fixed-top navbar-expand-lg navbar-dark default-color">
-        <a class="navbar-brand" href="<?php bloginfo('wpurl'); ?>"><strong><?php bloginfo('name') ?></strong></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <?php
-            wp_nav_menu( array(
-                'menu'              => 'primary',
-                'theme_location'    => 'primary',
-                'depth'             => 2,
-                'container'         => 'div',
-                'container_class'   => 'collapse navbar-collapse',
-                'container_id'      => 'bs-example-navbar-collapse-1',
-                'menu_class'        => 'nav navbar-nav',
-                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-                'walker'            => new WP_Bootstrap_Navwalker())
-            );
-        ?>
-          <ul class="navbar-nav">
-            <li class="nav-item" style="color: white;">
-              <strong><?php bloginfo('description') ?></strong>
-            </li>
-          </ul>
-        </div>
-      </nav>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'sf-theme' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$sf_theme_description = get_bloginfo( 'description', 'display' );
+			if ( $sf_theme_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $sf_theme_description; /* WPCS: xss ok. */ ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'sf-theme' ); ?></button>
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+			) );
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
